498af29e36d17ed073c57a27ff20cedbf0974cbc
Josh Okon <josh.okoncs11@gmail.com>
2024-02-15 22:55:51 -0500

    more styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import HomePage from "./components/home-page"
-import { Authentication } from "./components/authentication"
+import { Authentication } from "./components/authentication/authentication"
 
 function App() {
   return (
---
+++ src/components/authentication/authentication.jsx
@@ -0,0 +1,12 @@
+import { SignInForm } from "../sign-in-form/sign-in-form"
+import { SignUpForm } from "../sign-up-form/sign-up-form"
+import "./authentication.styles.scss"
+
+export const Authentication = () => {
+  return (
+    <div className="authentication-container">
+      <SignInForm />
+      <SignUpForm />
+    </div>
+  )
+}
