d9ddb11b7f18f1891652e997560ee4c3e6bf1bb6
Josh Okon <josh.okoncs11@gmail.com>
2024-04-26 19:30:10 -0400

    user-info component styling

--- src/components/user-info/user-info.jsx
+++ src/components/user-info/user-info.jsx
@@ -7,31 +7,35 @@ export const UserInfo = () => {
   const currentUser = useSelector((state) => state.user.currentUser)
 
   return (
-    <Card variant="outlined">
-      <CardContent>
-        <Typography variant="h5" component="h2">
-          Signed In User
-        </Typography>
-        {currentUser ? (
-          <div>
-            <Typography variant="body1">
-              <strong>Name:</strong> {currentUser.displayName}
-            </Typography>
-            <Typography variant="body1">
-              <strong>Email:</strong> {currentUser.email}
-            </Typography>
-            <Typography variant="body1">
-              <strong>UID:</strong> {currentUser.uid}
-            </Typography>
-            <Typography variant="body1">
-              <strong>Creation Date:</strong>{" "}
-              {new Date(currentUser.metadata.creationTime).toLocaleDateString()}
-            </Typography>
-          </div>
-        ) : (
-          <Typography variant="body1">Loading user information...</Typography>
-        )}
-      </CardContent>
-    </Card>
+    <div className="card-container">
+      <Card variant="outlined" className="card">
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Signed In As:
+          </Typography>
+          {currentUser ? (
+            <div>
+              <Typography variant="body1">
+                <strong>Name:</strong> {currentUser.displayName}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Email:</strong> {currentUser.email}
+              </Typography>
+              <Typography variant="body1">
+                <strong>UID:</strong> {currentUser.uid}
+              </Typography>
+              <Typography variant="body1">
+                <strong>Creation Date:</strong>{" "}
+                {new Date(
+                  currentUser.metadata.creationTime
+                ).toLocaleDateString()}
+              </Typography>
+            </div>
+          ) : (
+            <Typography variant="body1">Loading user information...</Typography>
+          )}
+        </CardContent>
+      </Card>
+    </div>
   )
 }
